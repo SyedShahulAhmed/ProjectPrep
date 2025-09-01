c22ef16cb92fd04d1d51886950dbd8380c19176e
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2025-09-01 20:55:46 +0530

    Added TaskModelSchema

---
+++ 01_Phase/server/models/TaskModel.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const taskSchema = mongoose.Schema(
+    {
+        text : {
+            type : String,
+            required : true,
+        },
+        completed : {
+            type : Boolean,
+            default : false
+        },
+    }
+)
+
+export default mongoose.model("Task",taskSchema);
