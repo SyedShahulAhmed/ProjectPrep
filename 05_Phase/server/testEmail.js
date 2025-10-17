37f5da06473390fc7656bfc9e279f3bcddbac93e
SHAHUL-AHMED-77 <shahul77235@gmail.com>
2025-10-18 00:48:38 +0530

    test succesfully completed

---
+++ 05_Phase/server/testEmail.js
@@ -0,0 +1,7 @@
+import { sendEmail } from "./email.js";
+
+sendEmail(
+  "shahul77235@gmail.com", // ← replace with your email or test one
+  "Hello from Daily Quote Bot",
+  "This is a test email from your automation bot setup!"
+);
